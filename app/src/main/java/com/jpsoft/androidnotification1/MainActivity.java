4539167233ed78fe92d9ab33e10e448f2914cc36
Junaed <junaed1995@gmail.com>
2022-03-13 09:54:24 +0600

    * PendingIntent added

--- app/src/main/java/com/jpsoft/androidnotification1/MainActivity.java
+++ app/src/main/java/com/jpsoft/androidnotification1/MainActivity.java
@@ -8,7 +8,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -32,23 +34,30 @@ public class MainActivity extends AppCompatActivity {
         binding.channel2Button.setOnClickListener(v -> channel_2_notification());
     }
 
+    // Channel 1 Notification
     private void channel_1_notification(){
         String title = binding.editTextTextTitle.getText().toString();
         String content = binding.editTextTextDescription.getText().toString();
         Uri soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://"+ getApplicationContext().getPackageName() + "/" + R.raw.message_notification);
 
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
 
         Notification notification = new NotificationCompat.Builder(this,CHANNEL_ID_1)
                 .setSmallIcon(R.drawable.ic_baseline_looks_one_24)
                 .setContentTitle(title)
                 .setContentText(content)
+                .setAutoCancel(true)
                 .setSound(soundUri)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(contentIntent)
                 .build();
         notificationManager.notify(1, notification);
     }
 
+    // Channel 2 Notification
     private void channel_2_notification(){
         String title = binding.editTextTextTitle.getText().toString();
         String content = binding.editTextTextDescription.getText().toString();
